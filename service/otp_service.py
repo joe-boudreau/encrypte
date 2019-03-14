a4ec42ff61b09063d28437d8b8add4b77d1a7bbf
anthonykemmeugne <anthonykemmeugne@gmail.com>
2019-03-14 13:53:38 -0400

    developping otp...

--- service/otp_service.py
+++ service/otp_service.py
@@ -4,21 +4,16 @@ import urllib.parse
 import pyqrcode
 
 
-def generate_QR_code(accountName, issuer, password):
-    b = bytes(password, 'utf-8')
-    encoded = base64.b32encode(b)
-    val = encoded.decode("utf-8")
-    URL = pyotp.totp.TOTP(val[:16]).provisioning_uri(accountName, issuer_name=issuer)
+def generate_QR_code(accountName, issuer):
+    password = pyotp.random_base32()
+    URL = pyotp.totp.TOTP(password).provisioning_uri(accountName, issuer_name=issuer)
     qrcode = pyqrcode.create(URL)
     qrcode.svg('uca-url.svg', scale=1)
     qrcode.eps('uca-url.eps', scale=2)
     print(qrcode.terminal(quiet_zone=1))
-
+    return password
 def verify_otp_password(password, otp):
-    b = bytes(password, 'utf-8')
-    encoded = base64.b32encode(b)
-    val = encoded.decode("utf-8")
-    totp = pyotp.TOTP(val[:16])
+    totp = pyotp.TOTP(password)
     print("Current OTP:", totp.now())
     if otp == totp.now():
         print('Password valid')
--- service/test_otp.py
+++ service/test_otp.py
@@ -2,12 +2,8 @@ import sys
 from service.otp_service import *
 
 accountname = input('Please enter your account name: ')
-password = input('Please enter your password : ')
-if (len(password)<10):
 
-    sys.exit("Error ! the password need to be at least 10 character long")
-else:
-    generate_QR_code(accountname, 'encrypte', password)
+password = generate_QR_code(accountname, 'encrypté')
 
-    otp = input('Enter OTP')
-    verify_otp_password(password, otp)
+otp = input('Enter OTP')
+verify_otp_password(password, otp)
