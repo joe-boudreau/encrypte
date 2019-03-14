bb38d21931bcd4dc92ec6918beabfb4cc66227ba
Anthony Kemmeugne <anthonykemmeugne@gmail.com>
2019-03-14 15:15:44 -0400

    generate png qr code

--- service/otp_service.py
+++ service/otp_service.py
@@ -2,14 +2,15 @@ import pyotp
 import base64
 import urllib.parse
 import pyqrcode
-
+import png
 
 def generate_QR_code(accountName, issuer):
     password = pyotp.random_base32()
     URL = pyotp.totp.TOTP(password).provisioning_uri(accountName, issuer_name=issuer)
     qrcode = pyqrcode.create(URL)
-    qrcode.svg('uca-url.svg', scale=1)
-    qrcode.eps('uca-url.eps', scale=2)
+    qrcode.png('Generated_QR_code.png', scale= 6) #Generate a png image of the qrcode
+    qrcode.svg('uca-url.svg', scale=8) #generate html file containing the QRcode
+    qrcode.eps('uca-url.eps', scale=4) #generate eps file
     print(qrcode.terminal(quiet_zone=1))
     return password
 def verify_otp_password(password, otp):
